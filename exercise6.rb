7d40cb4e365273099110094cb957313e0e9d01a4
Sam Sawchuk <samsawchuk@shaw.ca>
2015-06-10 15:02:46 -0400

    sorted exercise 6 list by alphabet

--- exercise6.rb
+++ exercise6.rb
@@ -4,10 +4,25 @@ def astlist(list)
 end
 
 groceries = ["carrots", "toilet paper", "apples", "salmon"]
-
+# index         0            1              2         3           
 	astlist groceries
 
 groceries << "rice"
 
 astlist groceries
+
+puts groceries.length
+
+if groceries.include? ("bananas") == false
+	puts "You need to pick up bananas"
+else
+	puts "You don't need to pick up bananas today"
+end
+
+puts groceries[1]
+
+puts groceries.sort
+
+
+
 
